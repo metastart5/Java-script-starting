19f18ffa4ae7ec7b44fb6e7016d642a6e355b6dd
metastart5 <150458215+metastart5@users.noreply.github.com>
2025-11-14 19:20:53 +0000

    data types

---
+++ 01basics/02datatype.js
@@ -0,0 +1,32 @@
+// alert(3+3);  //we are using nodejs not browser
+// we can use semicolon but its optional in js
+
+console.log(3 +3);
+console.log("Hello World");
+
+
+console.log(3 +6)
+
+console.log("Hello ")
+// we can use semicolon but its optional in js
+
+let name = "Akshat Aditya";
+let age = 22
+let isLoggedIn = false;
+let state; //undefined
+
+// number = 2 to power 53
+// string =""
+// boolean = true/false
+// null = standalone value
+// undefined = variable not assigned
+// symbol = unique identifier
+
+//object
+
+// null is a object in js
+
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isLoggedIn);
+console.log(typeof state);  

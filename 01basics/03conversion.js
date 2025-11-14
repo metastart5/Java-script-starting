1b915ff1d7a03bc20ab4a77fedb21999c0aab722
metastart5 <150458215+metastart5@users.noreply.github.com>
2025-11-14 19:39:53 +0000

    conversion

---
+++ 01basics/03conversion.js
@@ -0,0 +1,23 @@
+let score = 33
+
+console.log(typeof score);
+
+let valueinnumber = Number(score); // conversion
+console.log(typeof valueinnumber);
+console.log(valueinnumber);
+
+//"33" = 33
+//33abd = NaN
+//true = 1 , false =0
+
+let isLoggedIn = 1;
+let booleanvalue = Boolean(isLoggedIn);
+console.log(booleanvalue);
+console.log(typeof booleanvalue);
+
+// 1 = true
+// 0 = false
+// "" = false
+// "akshat" = true
+// null = false
+// undefined = false
